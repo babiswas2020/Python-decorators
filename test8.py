405df6062f49e9a729108ebdcfb85b449c57c908
Bapan <36biswas0814@gmail.com>
2020-07-08 10:05:43 +0530

    Commited

--- test8.py
+++ test8.py
@@ -1,8 +1,9 @@
 def isNumber(s):
-   for i in list(s):
-      if i.isdigit()!=True:
-         return False
-   return True
+   if s:
+      for i in list(s):
+        if i.isdigit()!=True:
+           return False
+      return True
 
 if __name__=="__main__":
   str1=input("enter your input")
